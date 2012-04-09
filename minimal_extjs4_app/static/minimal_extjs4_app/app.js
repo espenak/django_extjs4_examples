72fbd53fc2c9f16258d770e2a641f999a8c98c8c
Espen A. Kristiansen <post@espenak.net>
2012-04-09 22:12:21 +0200

    app.js: Added appFolder.

--- minimal_extjs4_app/static/minimal_extjs4_app/app.js
+++ minimal_extjs4_app/static/minimal_extjs4_app/app.js
@@ -1,5 +1,6 @@
 Ext.application({
     name: 'HelloExt',
+    appFolder: '/static/minimal_extjs4_app/app',
     launch: function() {
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
